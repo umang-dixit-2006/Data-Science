09e958686f69516a37eee3bf894ffb33131b77d8
Umang <212006umang@gmail.com>
2025-06-19 10:05:10 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+ar = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+sum = np.sum(ar)
+product= np.prod(ar)
+print("Sum:", sum)
+print("Product:", product)
+mean = np.mean(ar)
+print("Mean:", mean)
+length = len(ar)
+print("Length:", length)
